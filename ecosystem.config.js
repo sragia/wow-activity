f647fdbd74d8a963b42b51473eb147b07cc3cab9
Aigars Bedeicis <a.bedeicis@gmail.com>
2021-04-27 20:43:43 +0300

    add pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [{
+    name: 'wowactivity-api',
+    script: 'dist/main.js',
+    watch: 'backend/dist',
+    cwd: './backend',
+  },
+  {
+    name: 'wowactivity-fe',
+    script: 'serve',
+    env: {
+      PM2_SERVE_PATH: 'frontend/build',
+      PM2_SERVE_PORT: 3000,
+      PM2_SERVE_SPA: 'true',
+      PM2_SERVE_HOMEPAGE: '/index.html',
+    },
+  }
+  ],
+};
